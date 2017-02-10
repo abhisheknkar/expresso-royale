ad65ed66e2f31c5633ed3b8f617831a0d49f11a0
Abhishek Narwekar <Abhishek Narwekar>
2017-02-10 16:08:20 -0600

    Plotting the results for a few books

--- project01/code/analyzer.py
+++ project01/code/analyzer.py
@@ -4,7 +4,7 @@ import time
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-
+from itertools import cycle
 
 class EmotionAnalyzer():
     def __init__(self):
@@ -13,6 +13,9 @@ class EmotionAnalyzer():
 
     def analyze(self, filepath, preprocessed=True, window=0.01):
         # Preprocess the text or read from preprocessed text file
+        self.folderName = filepath[:filepath.rfind('/')+1]
+        self.textName = filepath[filepath.rfind('/')+1:-4]
+        self.window = window
         fileread = False
         cleaner = TextCleaner()
         if preprocessed:
@@ -39,9 +42,9 @@ class EmotionAnalyzer():
         processedContent = cleaner.removePunctuation(processedContent)  # Remove punctuations
         contentsplit = processedContent.split()
 
-        if window < 1:
-            window = int(window * len(contentsplit))
-        numWindows = int(np.ceil(len(contentsplit)/window))+1
+        if self.window < 1:
+            self.window = int(self.window * len(contentsplit))
+        numWindows = int(np.ceil(len(contentsplit)/self.window))+1
 
         # Create a dictionary to store the emotion intensities
         self.emotionDict = dict((el, np.zeros(numWindows)) for el in self.emotions)
@@ -50,7 +53,7 @@ class EmotionAnalyzer():
         windowID = 0
 
         while start < len(contentsplit):
-            end = min(start+window, len(contentsplit))
+            end = min(start+self.window, len(contentsplit))
             words = contentsplit[start:end]
             for word in words:
                 if word in self.nrcDict:
@@ -63,19 +66,35 @@ class EmotionAnalyzer():
 
 
     def plot(self):
-        x = np.arange(len(self.emotionDict[self.emotions[0]]))
-        plt.plot(x,x)
+        x = [float(y+1)/len(self.emotionDict[self.emotions[0]])*100 for y in np.arange(len(self.emotionDict[self.emotions[0]]))]
+        lines = ["-", "--", "-."]
+        linecycler = cycle(lines)
+
+        # colors = ['b','g','r','c','m','y','k']
+        # colorcycler = cycle(colors)
+
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111)
+        for emotion in self.emotionDict:
+            plt.plot(x[:-1], self.emotionDict[emotion][0:-1], next(linecycler))
+
+        plt.legend(self.emotions, loc='upper left')
+        plt.xlabel('Percentage')
+        plt.ylabel('Number of words (window length='+str(self.window)+')')
+        plt.title('Variation of emotions over: '+ self.textName)
+        # manager = plt.get_current_fig_manager()
+        # manager.window.showMaximized()
         plt.show()
-        # for emotion in self.emotionDict:
-        #     plt.plot(x, self.emotionDict[emotion])
-        #
-        # plt.legend(self.emotions, loc='upper left')
-        # plt.show()
+
+
+        # plt.savefig(self.folderName+self.textName+'.png')
 
 if __name__ == '__main__':
     t0 = time.time()
 
     e = EmotionAnalyzer()
-    e.analyze('../data/Books/PrideAndPrejudice.txt', window=0.01)
+    books = ['PrideAndPrejudice', 'AliceInWonderland', 'AdventuresOfSherlockHolmes']
+    for book in books:
+        e.analyze('../data/Books/'+book+'.txt', window=0.04)
 
     print 'Time taken: ', time.time()-t0
