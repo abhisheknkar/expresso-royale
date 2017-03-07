3c227bc113d00a2537344c138eed63550adbbe3d
Abhishek <abhisheknkar@gmail.com>
2017-03-07 09:13:15 -0600

    Cue analysis performed

--- project02/code/analyzer.py
+++ project02/code/analyzer.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*-
 import networkx as nx
+import numpy as np
+from preprocessor import *
+import csv
 
 class EmotionThesaurusBookAnalyzer():
     def __init__(self):
@@ -7,8 +11,13 @@ class EmotionThesaurusBookAnalyzer():
         self.graphPath = '../data/EmotionThesaurus/contingencyGraph.xml'
 
         self.emotionSet = self.getEmotionList()
-        self.getContingencyList(writeToFile=True)
-        self.createContingencyGraph(self.graphPath)
+
+        # self.getContingencyList(writeToFile=True)
+        # self.createContingencyGraph(self.graphPath)
+
+        self.generateCueDictionary()
+        self.generateReverseCueIndex()
+        # self.morphologicalAnalysis()
 
     def getEmotionList(self):
         f = open(self.emotionListPath,'r')
@@ -78,55 +87,106 @@ class EmotionThesaurusBookAnalyzer():
         nx.write_graphml(self.G,path)
         # print nx.cycle_basis(self.G)
 
+    def generateCueDictionary(self):
+        f = open(self.thesaurusPath,'r')
+        cueAttributes = ['DEFINITION','PHYSICAL SIGNALS', 'INTERNAL SENSATIONS', 'MENTAL RESPONSES', 'CUES OF ACUTE OR LONG-TERM', 'CUES OF SUPPRESSED', 'WRITER’S TIP']
+        title = ''
 
-def find_all_cycles(G, source=None, cycle_length_limit=None):
-    """forked from networkx dfs_edges function. Assumes nodes are integers, or at least
-    types which work with min() and > ."""
-    if source is None:
-        # produce edges for all components
-        nodes = [i[0] for i in nx.connected_components(G)]
-    else:
-        # produce edges for components with source
-        nodes = [source]
-    # extra variables for cycle detection:
-    cycle_stack = []
-    output_cycles = set()
+        self.emotionCueDict = {}
+        cueReverseIndex = {}
+        count = 0
 
-    def get_hashable_cycle(cycle):
-        """cycle as a tuple in a deterministic order."""
-        m = min(cycle)
-        mi = cycle.index(m)
-        mi_plus_1 = mi + 1 if mi < len(cycle) - 1 else 0
-        if cycle[mi - 1] > cycle[mi_plus_1]:
-            result = cycle[mi:] + cycle[:mi]
-        else:
-            result = list(reversed(cycle[:mi_plus_1])) + list(reversed(cycle[mi_plus_1:]))
-        return tuple(result)
+        for line in f.readlines():
+            newPageFlag = False
+            if ord(line[0]) == 12:
+                newPageFlag = True
+            lstrip = line.strip()
+            lsplit = lstrip.split()
 
-    for start in nodes:
-        if start in cycle_stack:
-            continue
-        cycle_stack.append(start)
+            if len(lsplit) == 0 or lstrip.startswith('Return to the Table of Contents'):
+                continue
 
-        stack = [(start, iter(G[start]))]
-        while stack:
-            parent, children = stack[-1]
-            try:
-                child = next(children)
+            if newPageFlag:
+                potentialTitle = lsplit[0]
+                if potentialTitle.lower() in self.emotionSet and potentialTitle.isupper():
+                    # title = potentialTitle.lower()
+                    title = potentialTitle
+                    self.emotionCueDict[title] = {}
+                    for att in cueAttributes:
+                        self.emotionCueDict[title][att] = []
 
-                if child not in cycle_stack:
-                    cycle_stack.append(child)
-                    stack.append((child, iter(G[child])))
-                else:
-                    i = cycle_stack.index(child)
-                    if i < len(cycle_stack) - 2:
-                        output_cycles.add(get_hashable_cycle(cycle_stack[i:]))
+            if len(lsplit[0]) > 1 and lsplit[0].isupper(): # A word larger than 1 char in caps
+                cueTypeFlag = [lstrip.startswith(x) for x in cueAttributes]
+                whereTrue = [idx for idx,x in enumerate(cueTypeFlag) if x]
+                if len(whereTrue) > 0:
+                    currAttribute = cueAttributes[whereTrue[0]]  # The current attribute to be considered
+                    if currAttribute == 'DEFINITION' or currAttribute == 'WRITER’S TIP': # Store the definition here, since it is in line
+                        self.emotionCueDict[title][currAttribute].append(lstrip[lstrip.find(': ') + 2:].lower())
 
-            except StopIteration:
-                stack.pop()
-                cycle_stack.pop()
+            else:
+                self.emotionCueDict[title][currAttribute].append(lstrip)
 
-    return [list(i) for i in output_cycles]
+    def generateReverseCueIndex(self,clean=False, toSort=False, toPrint=True):
+        # For each cue, preprocess, sort words alphabetically, store in dictionary
+        self.emotionCueRI = {}
+        cleaner = TextCleaner()
+        for emotion in self.emotionCueDict:
+            # for attribute in self.emotionCueDict[emotion]:
+            for attribute in ['PHYSICAL SIGNALS', 'INTERNAL SENSATIONS', 'MENTAL RESPONSES', 'CUES OF ACUTE OR LONG-TERM', 'CUES OF SUPPRESSED']:
+                for cue in self.emotionCueDict[emotion][attribute]:
+                    if clean:
+                        cleanCue = cleaner.clean(cleaner.removePunctuation(cue.lower()))
+                    else:
+                        cleanCue = cleaner.removePunctuation(cue.lower())
+                    if toSort:
+                        cleanCue = ' '.join(sorted(list(set(cleanCue.split()))))
 
+                    if cleanCue not in self.emotionCueRI:
+                        self.emotionCueRI[cleanCue] = []
+                    self.emotionCueRI[cleanCue].append((emotion,attribute))
+
+        # Print the output
+        if toPrint:
+            print "Total cues:", len(self.emotionCueRI)
+            count = 0
+            f1 = open('results/intersections.txt','w')
+            f2 = open('results/allcues.txt','w')
+            for cue in self.emotionCueRI:
+                f2.write(cue + '\t' + ','.join([str(x) for x in self.emotionCueRI[cue]]) + '\n')
+                if len(self.emotionCueRI[cue]) > 1:
+                    count += 1
+                    f1.write(cue + '\t' + ','.join([str(x) for x in self.emotionCueRI[cue]]) + '\n')
+            f1.close()
+            f2.close()
+            print "Cues in the intersection:", count
+
+    def morphologicalAnalysis(self):
+        pos = LemmatizerWithPOS()
+        posDist = {}
+        # for cue in self.emotionCueRI:
+        outFile = open('morphoDist.csv','w')
+        writer = csv.writer(outFile,delimiter=',')
+        writer.writerow(['Emotion','Other','Adjective','Adverb','Noun','Verb','Total Words'])
+
+        for emotion in self.emotionCueDict:
+            posDist[emotion] = {}
+            # for attribute in self.emotionCueDict[emotion]:
+            for attribute in ['PHYSICAL SIGNALS', 'INTERNAL SENSATIONS', 'MENTAL RESPONSES', 'CUES OF ACUTE OR LONG-TERM', 'CUES OF SUPPRESSED']:
+                for cue in self.emotionCueDict[emotion][attribute]:
+                    try:
+                        tokens = nltk.word_tokenize(cue)
+                    except:
+                        tokens = []
+                    tagged = nltk.pos_tag(tokens)
+                    for pair in tagged:
+                        category = pos.get_wordnet_pos(pair[1])
+                        if category not in posDist[emotion]:
+                            posDist[emotion][category] = 0
+                        posDist[emotion][category] += 1
+            totalWords = sum(posDist[emotion].values())
+            posDist[emotion] = {k: v*1.0/sum(posDist[emotion].values()) for k, v in posDist[emotion].iteritems()}
+
+            writer.writerow([emotion, posDist[emotion][''], posDist[emotion]['a'], posDist[emotion]['r'], posDist[emotion]['n'], posDist[emotion]['v'], totalWords])
+        outFile.close()
 
 analyzer = EmotionThesaurusBookAnalyzer()
